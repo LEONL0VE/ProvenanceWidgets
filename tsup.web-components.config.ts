068cffd35f208ed73fe099750ce81a67bd35c348
leon <leon459196355@gmail.com>
2026-08-13 22:28:35 +0800

    build: fold web component preparation into tsup

--- tsup.web-components.config.ts
+++ tsup.web-components.config.ts
@@ -1,4 +1,45 @@
 import { defineConfig } from "tsup";
+import { constants } from "node:fs";
+import { access, copyFile, rename, rm } from "node:fs/promises";
+
+const outputDirectory = new URL("./dist/web-components/", import.meta.url);
+
+const publishGeneratedFile = async (
+  generatedFile: URL,
+  publicFile: URL
+) => {
+  try {
+    await rename(generatedFile, publicFile);
+  } catch (error) {
+    if (
+      !error ||
+      typeof error !== "object" ||
+      !("code" in error) ||
+      error.code !== "EPERM"
+    ) {
+      throw error;
+    }
+    await copyFile(generatedFile, publicFile);
+    await rm(generatedFile, { force: true });
+  }
+};
+
+const prepareOutput = async () => {
+  const generatedScript = new URL("index.global.js", outputDirectory);
+  await access(generatedScript, constants.R_OK);
+  await copyFile(
+    new URL("./src/web-components/public.d.ts", import.meta.url),
+    new URL("index.d.ts", outputDirectory)
+  );
+  await publishGeneratedFile(
+    generatedScript,
+    new URL("index.js", outputDirectory)
+  );
+  await publishGeneratedFile(
+    new URL("index.css", outputDirectory),
+    new URL("styles.css", outputDirectory)
+  );
+};
 
 export default defineConfig({
   entry: {
@@ -14,6 +55,7 @@ export default defineConfig({
   sourcemap: true,
   minify: false,
   noExternal: [/.*/],
+  onSuccess: prepareOutput,
   esbuildOptions(options) {
     options.alias = {
       ...options.alias,
